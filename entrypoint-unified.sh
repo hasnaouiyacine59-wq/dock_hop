0d8786f2ab408488824916482a01e8037fc7c2b4
oo33 <hasnaouiyacine59@gmail.com>
2026-04-24 05:04:55 +0100

    fix_bulk1

--- entrypoint-unified.sh
+++ entrypoint-unified.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
+
+# Set timezone based on IP geolocation
+echo "Detecting timezone from IP..."
+TIMEZONE=$(curl -s http://ip-api.com/json | jq -r '.timezone' || echo "UTC")
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+echo $TIMEZONE > /etc/timezone
+echo "Timezone set to: $TIMEZONE"
+
 ##apt update && apt install git -y
+#docker login -u='mylastres0rt05_redhat' -p='4ukFQ9E2c1NOTM1FJ/edkLCc1uLlLWYary3DI5mgSAtB3y/RbFdHqOEjqDmzPfJm' quay.io
 git clone https://github.com/hasnaouiyacine59-wq/dock_hop.git 
 Xvfb :1 -screen 0 1920x1080x24 &
 sleep 1
